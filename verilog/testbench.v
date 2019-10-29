7282ac44e1b70a2fdbebc8519ba21fbd0a892e99
Ole Sivert Aarhaug <ole.sivert@aarhaug.com>
2019-10-29 15:35:17 +0100

    more

--- verilog/testbench.v
+++ verilog/testbench.v
@@ -9,3 +9,19 @@ module basic_test_model();
     end
 
 endmodule
+
+
+module FSM_ex_control(init, clk, RESET, NRE_1, NRE_2, ADC, Expose, Erase);
+
+    input init, clk, RESET;
+    output NRE_1, NRE_2, ADC, Expose, Erase;
+
+    reg idle;
+    
+
+    always_ff @(posedge clk) begin
+        
+    end
+
+
+endmodule
