a6b561f380cf975bb5561632c24f770abd4cb774
Ole Sivert Aarhaug <ole.sivert@aarhaug.com>
2019-10-29 16:08:52 +0100

    fixed syntax errors

---
+++ verilog/verilogWorkbench/icDesign/src/CTRL_ex_time.v
@@ -0,0 +1,22 @@
+module CTRL_ex_time(Exp_increase, Exp_decrease, Clk, Reset, EX_time);
+
+    input Exp_increase, Exp_decrease, Clk, Reset;
+    output EX_time;
+
+    reg[0:5] EX_time;
+
+    initial begin
+        EX_time = 10;
+    end
+    always @ (posedge Exp_increase)begin
+        if (EX_time < 30) begin
+            EX_time++;        
+        end
+    end
+    always @ (posedge Exp_decrease)begin
+        if (EX_time > 2) begin
+            EX_time--;
+        end
+    end
+
+endmodule
